a26880ed6f65f5348fa1de2dafa3c222ca51b334
aurora-kuczek <rbkuczek13@gmail.com>
2022-11-19 14:43:20 -0500

    GEOPANDAS

--- Open_Jupyter_gis.bat
+++ Open_Jupyter_gis.bat
@@ -1,4 +1,5 @@
-@set the_env=gis
+@set the_env=gis5
 call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\activate.bat" "%the_env%"
-call "%localappdata%\ESRI\conda\envs\%the_env%\scripts\jupyter-notebook.exe" %cd% 
+call "%localappdata%\ESRI\conda\envs\env859-py3\envs\%the_env%\scripts\jupyter-notebook.exe" %cd% 
 call "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\deactivate.bat"
+pause
